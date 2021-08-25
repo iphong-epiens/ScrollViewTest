049437fe2e44c83c8435123718fddba86f8971d3
Inpyo Hong <y2kpaulh@epiens.com>
2021-08-25 19:18:52 +0900

    ScrollViewOffset

--- ScrollViewTest/ContentView.swift
+++ ScrollViewTest/ContentView.swift
@@ -7,60 +7,142 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State private var listData: [ToDoItem] = []
+//ScrollViewOffset https://www.fivestars.blog/articles/scrollview-offset/
+//example 1
 
-    var body: some View {
-        
-        ScrollViewReader { scrollView in
-            
-            ScrollView(
-                axes: [.vertical],
-                showsIndicators: false,
-                offsetChanged: {
-                    print("offsetChanged", $0)
-                }
-            ) {
-                Button(action: {
-                    print("tap button")
-                    self.listData.append(ToDoItem(task: "task\(listData.count+1)\nDonec id elit non mi porta gravida at eget metus.", imageName: "command"))
-                    
-                }, label: {
-                    Text("add msg")
-                })
-                
-                Button("Scroll to bottom") {
-                    withAnimation {
-                        scrollView.scrollTo(0, anchor: .top)
-                    }
-                }
-                
-                LazyVStack {
-                    ForEach(0..<listData.count, id: \.self) { i in
-                        TestListCell(listData[i])
-                            .id(i)
-                            .flippedUpsideDown()
-                        
-                    }.padding()
-                }.flippedUpsideDown()
-                .onChange(of: listData, perform: { value in
-                    print("listData.count",listData.count)
-                })
-                .onAppear{
-                    for index in 1...100 {
-                        listData.append(ToDoItem(task: "task\(index)", imageName: "command"))
-                    }
-                }
-                
-                Button("Scroll to top") {
-                    withAnimation {
-                        scrollView.scrollTo(listData.count-1, anchor: .bottom)
-                    }
-                }
-            }
-        }
+struct ContentView: View {
+  @State private var scrollOffset: CGFloat = .zero
+
+  var body: some View {
+    ZStack {
+      scrollView
+      statusBarView
     }
+  }
+
+  var scrollView: some View {
+    ScrollViewOffset {
+      scrollOffset = $0
+    } content: {
+      LazyVStack {
+        ForEach(0..<100) { index in
+          Text("\(index)")
+        }
+      }
+    }
+  }
+
+  var statusBarView: some View {
+    GeometryReader { geometry in
+      Color.red
+        .opacity(opacity)
+        .frame(height: geometry.safeAreaInsets.top, alignment: .top)
+        .edgesIgnoringSafeArea(.top)
+    }
+  }
+
+  var opacity: Double {
+    switch scrollOffset {
+    case -100...0:
+      return Double(-scrollOffset) / 100.0
+    case ...(-100):
+      return 1
+    default:
+      return 0
+    }
+  }
 }
+
+//example 2
+
+//struct ContentView: View {
+//  @State var scrollOffset: CGFloat = .zero
+//
+//  var body: some View {
+//    ZStack {
+//      backgroundColor
+//      scrollView
+//    }
+//  }
+//
+//  var backgroundColor: some View {
+//    Color(
+//      //         This number determines how fast the color changes 👇🏻
+//      hue: Double(abs(scrollOffset.truncatingRemainder(dividingBy: 3500))) / 3500,
+//      saturation: 1,
+//      brightness: 1
+//    )
+//    .ignoresSafeArea()
+//  }
+//
+//  var scrollView: some View {
+//    ScrollViewOffset {
+//      scrollOffset = $0
+//    } content: {
+//      LazyVStack(spacing: 8) {
+//        ForEach(0..<100) { index in
+//          Text("\(index)")
+//            .font(.title)
+//        }
+//      }
+//    }
+//  }
+//}
+
+//struct ContentView: View {
+//    @State private var listData: [ToDoItem] = []
+//
+//    var body: some View {
+//
+//        ScrollViewReader { scrollView in
+//
+//            ScrollView(
+//                axes: [.vertical],
+//                showsIndicators: false,
+//                offsetChanged: {
+//                    print("offsetChanged", $0)
+//                }
+//            ) {
+//                Button(action: {
+//                    print("tap button")
+//                    self.listData.append(ToDoItem(task: "task\(listData.count+1)\nDonec id elit non mi porta gravida at eget metus.", imageName: "command"))
+//
+//                }, label: {
+//                    Text("add msg")
+//                })
+//
+//                Button("Scroll to bottom") {
+//                    withAnimation {
+//                        scrollView.scrollTo(0, anchor: .top)
+//                    }
+//                }
+//
+//                LazyVStack {
+//                    ForEach(0..<listData.count, id: \.self) { i in
+//                        TestListCell(listData[i])
+//                            .id(i)
+//                            .flippedUpsideDown()
+//
+//                    }.padding()
+//                }.flippedUpsideDown()
+//                .onChange(of: listData, perform: { value in
+//                    print("listData.count",listData.count)
+//                })
+//                .onAppear{
+//                    for index in 1...100 {
+//                        listData.append(ToDoItem(task: "task\(index)", imageName: "command"))
+//                    }
+//                }
+//
+//                Button("Scroll to top") {
+//                    withAnimation {
+//                        scrollView.scrollTo(listData.count-1, anchor: .bottom)
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ ScrollViewTest/ScrollViewOffset/ScrollViewOffset.swift
@@ -0,0 +1,49 @@
+//
+//  ScrollViewOffset.swift
+//  ScrollViewTest
+//
+//  Created by Inpyo Hong on 2021/08/25.
+//
+
+import SwiftUI
+
+//https://www.fivestars.blog/articles/scrollview-offset/
+
+struct ScrollViewOffset<Content: View>: View {
+  let onOffsetChange: (CGFloat) -> Void
+  let content: () -> Content
+
+  init(
+    onOffsetChange: @escaping (CGFloat) -> Void,
+    @ViewBuilder content: @escaping () -> Content
+  ) {
+    self.onOffsetChange = onOffsetChange
+    self.content = content
+  }
+
+  var body: some View {
+    ScrollView {
+      offsetReader
+      content()
+        .padding(.top, -8)
+    }
+    .coordinateSpace(name: "frameLayer")
+    .onPreferenceChange(OffsetPreferenceKey.self, perform: onOffsetChange)
+  }
+
+  var offsetReader: some View {
+    GeometryReader { proxy in
+      Color.clear
+        .preference(
+          key: OffsetPreferenceKey.self,
+          value: proxy.frame(in: .named("frameLayer")).minY
+        )
+    }
+    .frame(height: 0)
+  }
+}
+
+private struct OffsetPreferenceKey: PreferenceKey {
+  static var defaultValue: CGFloat = .zero
+  static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {}
+}
